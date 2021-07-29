655684ed410792a907d335e6a5e0e4104d7658bb
ADebnath98 <36085832+ADebnath98@users.noreply.github.com>
2021-07-29 15:41:45 +0530

    Add files via upload

---
+++ QRCODE.py
@@ -0,0 +1,14 @@
+import qrcode
+qr = qrcode.QRCode(
+
+	version=1,
+	box_size=10,
+	border=5
+
+	)
+
+data ="I am Aniket Debnath"
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black",back_color="white")
+img.save("1.png")
